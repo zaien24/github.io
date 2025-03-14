35d789f313b304662efae5ef635f4dbe0522934b
한성길 <zaien24@naver.com>
2025-03-15 01:44:01 +0900

    add 1

--- dev/0205/1.java
+++ dev/0205/1.java
@@ -146,3 +146,67 @@ public class GamePrototypeExample {
 }
 
 //✅ 이점: 새로운 캐릭터를 추가할 때 new 키워드를 사용하지 않고 복제하여 성능을 최적화할 수 있음.
+
+
+
+----
+class DeepPrototype implements Cloneable {
+    private String name;
+    private NestedObject nestedObject;
+
+    public DeepPrototype(String name, String nestedData) {
+        this.name = name;
+        this.nestedObject = new NestedObject(nestedData);
+    }
+
+    @Override
+    public DeepPrototype clone() {
+        return new DeepPrototype(this.name, this.nestedObject.getData());
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNestedData(String data) {
+        this.nestedObject.setData(data);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNestedData() {
+        return nestedObject.getData();
+    }
+
+    static class NestedObject {
+        private String data;
+
+        public NestedObject(String data) {
+            this.data = data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public String getData() {
+            return data;
+        }
+    }
+}
+
+// 클라이언트 코드
+public class DeepPrototypeExample {
+    public static void main(String[] args) {
+        DeepPrototype original = new DeepPrototype("Original", "Nested Data");
+        DeepPrototype cloned = original.clone();
+
+        // 원본 수정
+        original.setNestedData("Changed Nested Data");
+
+        System.out.println("Original Nested Data: " + original.getNestedData());
+        System.out.println("Cloned Nested Data: " + cloned.getNestedData());
+    }
+}
